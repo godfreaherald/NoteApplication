9b8ab99eb55c3e3f287666ef7b1e60a5db04e457
godfreaherald <wambusiness2015@gmail.com>
2018-08-03 15:39:53 +0300

    Update EasyNotesApplication.java

--- src/main/java/com/example/easynotes/EasyNotesApplication.java
+++ src/main/java/com/example/easynotes/EasyNotesApplication.java
@@ -2,16 +2,39 @@ package com.example.easynotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.*;
+
 
 
 
 @SpringBootApplication
-//@EnableJpaAuditing
-public class EasyNotesApplication {
+@EnableJpaAuditing
+/*The main class for war file packaging
+ * You have to add below code to POM.xml either as properties or in the plugins section
+ * To support two main classes in this section 
+ * extending SpringBootServletInitializer enables war file packaging
+ */
+
+public class EasyNotesApplication extends SpringBootServletInitializer {
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EasyNotesApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(EasyNotesApplication.class, args);
+    }
+
+}
+
+//The main class for jar file packaging
+/*public class EasyNotesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasyNotesApplication.class, args);
 	}
 }
+*/
